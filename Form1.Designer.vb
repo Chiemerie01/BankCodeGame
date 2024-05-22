ecac2ccf537a2aaf37646acb6d930f8b2f6dfc7a
Peniel Chiemerie Okeke <66641750+Chiemerie01@users.noreply.github.com>
2024-05-22 03:20:12 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnSubmit = New Button()
+        txtGuess = New TextBox()
+        lblFeedback = New Label()
+        btnStart = New Button()
+        lstGuesses = New ListBox()
+        SuspendLayout()
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(525, 174)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(102, 44)
+        btnSubmit.TabIndex = 0
+        btnSubmit.Text = "Submit"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' txtGuess
+        ' 
+        txtGuess.Location = New Point(219, 133)
+        txtGuess.Name = "txtGuess"
+        txtGuess.Size = New Size(265, 23)
+        txtGuess.TabIndex = 1
+        ' 
+        ' lblFeedback
+        ' 
+        lblFeedback.AutoSize = True
+        lblFeedback.Location = New Point(153, 136)
+        lblFeedback.Name = "lblFeedback"
+        lblFeedback.Size = New Size(60, 15)
+        lblFeedback.TabIndex = 2
+        lblFeedback.Text = "Feedback:"
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(61, 85)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(102, 44)
+        btnStart.TabIndex = 3
+        btnStart.Text = "Start"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' lstGuesses
+        ' 
+        lstGuesses.FormattingEnabled = True
+        lstGuesses.ItemHeight = 15
+        lstGuesses.Location = New Point(289, 271)
+        lstGuesses.Name = "lstGuesses"
+        lstGuesses.Size = New Size(120, 94)
+        lstGuesses.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lstGuesses)
+        Controls.Add(btnStart)
+        Controls.Add(lblFeedback)
+        Controls.Add(txtGuess)
+        Controls.Add(btnSubmit)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents txtGuess As TextBox
+    Friend WithEvents lblFeedback As Label
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lstGuesses As ListBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Private secretCode As String
+    Private attempts As Integer
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        StartGame()
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim guess As String = txtGuess.Text 'Converts input in textbox from string to numerical value
+        If IsValidGuess(guess) Then
+            attempts += 1
+            Dim feedback As String = GetFeedback(guess)
+            lblFeedback.Text = feedback
+            lstGuesses.Items.Add($"Attempt {attempts}: {guess} - {feedback}")
+            If feedback.Contains("4 correct and in correct position") Then
+                MessageBox.Show($"Congratulations! You've unlocked the box in {attempts} attempts")
+                btnSubmit.Enabled = False
+            End If
+
+        Else
+            MessageBox.Show("Invalid guess. Please enter 2 to 4 non-repititive digits between 1 and 9.")
+
+        End If
+    End Sub
+
+
+    Private Sub StartGame()
+        secretCode = GenerateSecretCode()
+        attempts = 0
+        lblFeedback.Text = String.Empty
+        txtGuess.Text = String.Empty
+        lstGuesses.Items.Clear()
+        btnSubmit.Enabled = True
+        MessageBox.Show("New game started! Enter your guess.")
+
+    End Sub
+
+    Private Function GenerateSecretCode() As String
+        Dim digits As List(Of Integer) = Enumerable.Range(1, 9).ToList()
+        Dim rnd As New Random()
+        Dim codelength As Integer = rnd.Next(2, 5)
+        Dim code As String = String.Empty
+
+        For i As Integer = 1 To codelength
+            Dim index As Integer = rnd.Next(digits.Count)
+            code &= digits(index).ToString()
+            digits.RemoveAt(index)
+        Next
+        Return code
+    End Function
+
+    Private Function IsValidGuess(guess As String) As Boolean
+        If guess.Length < 2 OrElse guess.Length > 4 Then
+            Return False
+        End If
+
+        For Each ch As Char In guess
+            If Not Char.IsDigit(ch) OrElse ch = "0"c OrElse guess.Count(Function(C) C = ch) > 1 Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+    Private Function GetFeedback(guess As String) As String
+        Dim correct As Integer = 0
+        Dim correctPosition As Integer = 0
+
+        For i As Integer = 0 To Math.Min(guess.Length, secretCode.Length) - 1
+            If secretCode.Contains(guess(i)) Then
+                correct += 1
+                If secretCode(i) = guess(i) Then
+                    correctPosition += 1
+                End If
+            End If
+        Next
+
+        Return $"{correct} correct and {correctPosition} in correct position"
+
+    End Function
+
+End Class
